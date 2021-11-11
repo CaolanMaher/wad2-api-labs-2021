d20d54815429d606a9eebc2ccafc158442c8b6b4
Caolan Maher <caolanmaher2@gmail.com>
2021-11-11 15:43:35 +0000

    Added routing for movies and movie reviews

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+//import genres from './api/genres';
 
 dotenv.config();
 
@@ -13,6 +14,8 @@ app.use(express.static('public'));
 app.use(express.json());
 
 app.use('/api/movies', moviesRouter);
+
+//app.use('/api/genres', genres);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
