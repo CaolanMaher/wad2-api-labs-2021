cf8adda005cffb50629bbb95f97e00dc94984995
Caolan Maher <caolanmaher2@gmail.com>
2021-12-10 20:02:00 +0000

    Topic 10 - Exercise

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -19,8 +19,13 @@ router.post('/',asyncHandler( async (req, res, next) => {
       return next();
     }
     if (req.query.action === 'register') {
-      await User.create(req.body);
-      res.status(201).json({code: 201, msg: 'Successful created new user.'});
+        if(req.body.password === /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/) {
+            await User.create(req.body);
+            res.status(201).json({code: 201, msg: 'Successful created new user.'});
+        }
+        else {
+            res.status(401).json({code: 401,msg: 'Password Format Incorrect'});
+        }
     } else {
       const user = await User.findByUserName(req.body.username);
         if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
@@ -56,9 +61,15 @@ router.post('/:userName/favourites', asyncHandler(async (req, res) => {
     const userName = req.params.userName;
     const movie = await movieModel.findByMovieDBId(newFavourite);
     const user = await User.findByUserName(userName);
-    await user.favourites.push(movie._id);
-    await user.save(); 
-    res.status(201).json(user); 
+
+    if(!user.favourites.includes(movie._id)) {
+        await user.favourites.push(movie._id);
+        await user.save(); 
+        res.status(201).json(user); 
+    }
+    else {
+        res.status(404).json({ code: 404, msg: 'Movie Already In Favourites' });
+    }
   }));
 
   router.get('/:userName/favourites', asyncHandler( async (req, res) => {
