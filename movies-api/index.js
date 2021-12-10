52ba2b433d7e01ff4b56fde52eaabe90c4b7cde2
Caolan Maher <caolanmaher2@gmail.com>
2021-12-10 15:18:20 +0000

    Mongo lab Users Router

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,6 +3,7 @@ import './db';
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import usersRouter from './api/users';
 //import genres from './api/genres';
 
 dotenv.config();
@@ -18,6 +19,9 @@ app.use(express.json());
 app.use('/api/movies', moviesRouter);
 
 //app.use('/api/genres', genres);
+
+//Users router
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
